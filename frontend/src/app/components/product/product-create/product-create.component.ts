f7399ebb99d584e80a2f692cf96f265795c8a2ad
Priscila Roza <priscilarozasilva@gmail.com>
2021-12-05 17:54:47 -0300

    feture[form created]

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
-import { ProductService } from './../product.service';
-import { Router } from "@angular/router";
+import { Product } from "./../product.model";
+import { ProductService } from "./../product.service";
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-product-create",
@@ -8,13 +9,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./product-create.component.css"],
 })
 export class ProductCreateComponent implements OnInit {
+  product: Product = {
+    name: "",
+    price: "",
+  };
+
   constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {}
+
   createProduct(): void {
-    this.productService.showMenssage("Produto criado com sucesso!");
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage("Produto criado!");
+      this.router.navigate(["/products"]);
+    });
   }
+
   cancel(): void {
-    this.router.navigate(['/products'])
+    this.router.navigate(["/products"]);
   }
 }
--- frontend/src/app/components/product/product.model.ts
+++ frontend/src/app/components/product/product.model.ts
@@ -1,8 +1,16 @@
+
+   
 export interface Product {
-    id?: number,
-    photo: string,
-    name: string,
-    description: string,
-    price: number,
+    id?: number
+    name: string
+    price: string
 }
+
+// export interface Product {
+//     id?: number,
+//     photo?: string,
+//     name: string,
+//     description?: string,
+//     price: number,
+// }
 // o sinal de interrogação torna o item opcional
