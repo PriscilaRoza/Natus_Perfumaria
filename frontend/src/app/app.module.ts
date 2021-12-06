3325c7af5c3d98a518fd77572275f756d5e8d1c6
Priscila Roza <priscilarozasilva@gmail.com>
2021-12-06 13:44:30 -0300

    update[form]

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -23,8 +23,6 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
-// import { ProductReadComponent } from "./components/product/product-read/product-read.component";
-// import { ProductRead2Component } from "./components/product/product-read2/product-read2.component";
 import { MatTableModule } from "@angular/material/table";
 import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatSortModule } from "@angular/material/sort";
@@ -33,6 +31,8 @@ import { RedDirective } from "./directives/red.directive";
 import localePt from "@angular/common/locales/pt";
 import { registerLocaleData } from "@angular/common";
 import { ForDirective } from "./directives/for.directive";
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+// import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
 // import { ProductUpdateComponent } from "./components/product/product-update/product-update.component";
 // import { ProductDeleteComponent } from "./components/product/product-delete/product-delete.component";
 
@@ -47,10 +47,11 @@ registerLocaleData(localePt);
     HomeComponent,
     ProductCrudComponent,
     ProductCreateComponent,
-    // ProductReadComponent,
-    // ProductRead2Component,
+    ProductReadComponent,
     RedDirective,
     ForDirective,
+    ProductReadComponent,
+    // ProductRead2Component,
     // ProductUpdateComponent,
     // ProductDeleteComponent,
   ],
--- frontend/src/app/components/product/product.model.ts
+++ frontend/src/app/components/product/product.model.ts
@@ -1,16 +1,8 @@
-
-   
 export interface Product {
-    id?: number
-    name: string
-    price: string
+    id?: number,
+    photo?: string,
+    name: string,
+    description?: string,
+    price?: string,
 }
-
-// export interface Product {
-//     id?: number,
-//     photo?: string,
-//     name: string,
-//     description?: string,
-//     price: number,
-// }
 // o sinal de interrogação torna o item opcional
